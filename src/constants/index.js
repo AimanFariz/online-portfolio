7b584722a0c5813761231f7e79692c311892987b
AimanFariz <aimanfarizz33@gmail.com>
2026-03-20 22:24:35 -0500

    added ctrl shift del

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,69 +23,76 @@ export const navLinks = [
   export const personalProjects = [
     {
       id: 1,
-      title:"Gameplanning for 2-High Coverage",
+      title:"Ctrl, Shift, Delete: Analysis of the 2023 MLB infield rule changes",
       category:"Sports Analytics",
       image:"/assets/blitz-thumbnail.png",
       link:"https://drive.google.com/file/d/10ktK4mZAmEQPRPSqT5EaednjJWCRiVwe/view?usp=sharing"
     },
     {
       id: 2,
+      title:"Gameplanning for 2-High Coverage",
+      category:"Sports Analytics",
+      image:"/assets/blitz-thumbnail.png",
+      link:"https://drive.google.com/file/d/10ktK4mZAmEQPRPSqT5EaednjJWCRiVwe/view?usp=sharing"
+    },
+    {
+      id: 3,
       title:"Tulsa Athletics Mobile App",
       category:"Design",
       image:"/assets/tulsa-athletics.jpg",
       link:"https://www.figma.com/proto/HuXqbeMQNAQzjiVUdgNTYG/Tulsa-Hurricane-Mobile-App?page-id=95%3A6492&node-id=1006-1142&starting-point-node-id=1006%3A1142&t=eAmMJqAH8BlCs7L9-1"
     },
     {
-      id: 3,
+      id: 4,
       title:"Pickoff Probability Model",
       category:"Sports Analytics",
       image:"/assets/baseball.jpg",
       link:"https://drive.google.com/file/d/1BLgyWDHDEcheHo4JaXUp1A8RMdXFVbQU/view?usp=sharing"
     },
     {
-      id: 4,
+      id: 5,
       title:"Chelsea FC 2025 Viz a Thon",
       category:"Design",
       image:"/assets/chelsea.png",
       link:"https://www.figma.com/proto/aeWLfoo0bYma3h7tQoOOaR/Chelsea-2025-Viz-a-thon---Aiman?page-id=2%3A5&node-id=36-633&viewport=178%2C60%2C0.19&t=mB4Fg6MFVdTThpOe-1&scaling=contain&content-scaling=fixed"
     },
     {
-      id: 5,
+      id: 6,
       title:"Craftcha",
       category:"Web Development",
       image:"/assets/craftcha.png",
       link:"https://github.com/AimanFariz/craftcha"
     },
     {
-      id: 6,
+      id: 7,
       title:"CareerCrisp",
       category:"Web Development",
       image:"/assets/career-crisp.png",
       link:"https://careercrisp.vercel.app/"
     },
     {
-      id: 7,
+      id: 8,
       title:"Oopsie Daisy",
       category:"Web Development",
       image:"/assets/oopsie.png",
       link:"https://oopsiedaisy.vercel.app/"
     },
     {
-      id: 8,
+      id: 9,
       title:"MLS Drafts Analysis",
       category:"Sports Analytics",
       image:"/assets/mls.webp",
       link: "https://medium.com/@aimanfarizz/mls-draft-analysis-which-position-has-the-best-odds-in-the-drafts-74950c83adfe"
     },
     {
-      id: 9,
+      id: 10,
       title:"Copa America Shot Analysis",
       category:"Sports Analytics",
       image:"/assets/copa.webp",
       link:"https://medium.com/@aimanfarizz/copa-america-2024-shot-analysis-7c51193e161f"
     },
     {
-      id: 10,
+      id: 11,
       title:"Don Pollo Quiz",
       category:"Web Development",
       image:"/assets/don-quiz.jpg",
